29e8726f0e6fa3f8b733c78e89fb319591591c2e
krackd <43828895+krackd@users.noreply.github.com>
2019-06-29 12:47:34 +0200

    Add Maget on resources

---
+++ Assets/MagnetOnTriggerEnter.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MagnetOnTriggerEnter : MonoBehaviour {
+
+	public string TargetTag = "Player";
+	public float MoveSpeed = 0.1f;
+
+	private GameObject target;
+
+	private void OnTriggerEnter(Collider other)
+	{
+		if (other.gameObject.CompareTag(TargetTag))
+		{
+			target = other.gameObject;
+		}
+	}
+
+	private void OnTriggerExit(Collider other)
+	{
+		if (other.gameObject.CompareTag(TargetTag))
+		{
+			target = null;
+		}
+	}
+
+	private void Update()
+	{
+		if (target != null)
+		{
+			Vector3 diff = target.transform.position - transform.position;
+			Vector3 lerp = Vector3.Lerp(Vector3.zero, diff, MoveSpeed * Time.deltaTime * 1000);
+			transform.Translate(lerp);
+		}
+	}
+}
